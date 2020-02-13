7743e695679d528be6576b6b62cf144add660c0c
Jdalvarezo <jhonatand_alvarez@hotmail.com>
2020-02-13 15:06:44 -0500

    All basic application functionality was coded

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
   // remove application menu
-  // mainWindow.setMenu(null)
+  mainWindow.setMenu(null)
   mainWindow.resizable = false;
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
--- js/calculator.js
+++ js/calculator.js
@@ -2,22 +2,6 @@ window.addEventListener("load", function() {
 
     var operandsBox = document.getElementById('operands');
     var resultBox = document.getElementById('result');
-
-    operandsBox.value = "";
-    resultBox.value = 0;
-
-    function insert(num) {
-        if (operandsBox.value == "") {
-            operandsBox.value = num;
-        } else {
-            operandsBox.value += num;
-        }
-    }
-    
-    function equal() {
-        var exp = operandsBox.value;
-        resultBox.value = eval(exp);
-    }
     
     function clean() {
         operandsBox.value = "";
@@ -28,56 +12,46 @@ window.addEventListener("load", function() {
         var exp = operandsBox.value;
         operandsBox.value = exp.substring(0, exp.length-1);
     }
-
-    var numeros = ["btn0", "btn1", "btn2", "btn3", "btn4", "btn5", "btn6", "btn7", "btn8", "btn9"];
-
-    for(i=0; i<numeros.length; i++) {
-        document.getElementById(numeros[i]).addEventListener("click", function() {
-            insert(i);
-        });
+    
+    function insert(num) {
+        if (operandsBox.value == "") {
+            operandsBox.value = num;
+        } else {
+            operandsBox.value += num;
+        }
     }
     
-    document.getElementById('btnClean').addEventListener("click", function() {
-        clean();
-    });
-    document.getElementById('btnBackspace').addEventListener("click", function() {
-        backspace();
-    });
-    document.getElementById('btnAdd').addEventListener("click", function() {
-        insert("+");
-    });
-    document.getElementById('btnSubtract').addEventListener("click", function() {
-        insert("-");
-    });
-    document.getElementById('btnMultiply').addEventListener("click", function() {
-        insert("*");
-    });
-    document.getElementById('btnDivide').addEventListener("click", function() {
-        insert("/");
-    });
-    document.getElementById('btnDot').addEventListener("click", function() {
-        insert(".");
-    });
-    document.getElementById('btnEqual').addEventListener("click", function() {
-        equal();
-    });
+    function equal() {
+        var exp = operandsBox.value.replace(/×|÷/gi, function(match) { return (match == '×') ? '*' : '/'; });
+        try {
+            resultBox.value = eval(exp);
+        } catch(error) {
+            resultBox.value = error;
+        }
+    }
 
-    // var btn7 = document.getElementById('btn7');
-    // var btn8 = document.getElementById('btn8');
-    // var btn9 = document.getElementById('btn9');
+    function addClickEvent(id, value){
+        document.getElementById(id).addEventListener("click", function() {
+            insert(value);
+        })
+    }
+
+    var numeros = ["btn0", "btn1", "btn2", "btn3", "btn4", "btn5", "btn6", "btn7", "btn8", "btn9"];
+    var operations = ["btnAdd", "btnSubtract", "btnMultiply", "btnDivide", "btnDot"];
+    var symbols = ["+", "-", "×", "÷", "."];
+
+    for(var i=0; i<numeros.length; i++) {
+        addClickEvent(numeros[i], i);
+    }
+
+    for(var i=0; i<operations.length; i++) {
+        addClickEvent(operations[i], symbols[i]);
+    }
+
+    document.getElementById('btnClean').addEventListener("click", function() { clean(); });
+    document.getElementById('btnBackspace').addEventListener("click", function() { backspace(); });
+    document.getElementById('btnEqual').addEventListener("click", function() { equal(); });
+
+    clean();
     
-
-    // var btn4 = document.getElementById('btn4');
-    // var btn5 = document.getElementById('btn5');
-    // var btn6 = document.getElementById('btn6');
-    
-
-    // var btn1 = document.getElementById('btn1');
-    // var btn2 = document.getElementById('btn2');
-    // var btn3 = document.getElementById('btn3');
-    
-
-    // var btn0 = document.getElementById('btn0');
-    
-
 });
